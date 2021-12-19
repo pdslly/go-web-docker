c3fa26b44da8df2513ab63c71e0fe807c7c282c8
pdslly <admin@ifront.net>
2021-12-19 14:08:31 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/joho/godotenv/autoload" //自动加载env文件
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "hello world"})
+	})
+	r.Run(os.Getenv("APP_PORT"))
+}
